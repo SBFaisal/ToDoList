e4c8b3cb3fd3df5755b2f474cfe924c929160364
SBFaisal <sbfaisal0786@gmail.com>
2024-05-05 14:05:01 +0530

    added controller in app

--- src/app.js
+++ src/app.js
@@ -3,25 +3,8 @@ const express = require('express');
 
 // Creating an Express application instance
 const app = express();
+const PORT = 3000;
 
-
-/* // Sample data
-const users = [
-    { id: '20IT001', name: 'Faisal' },
-    { id: '20IT002', name: 'Aarif' },
-    { id: '20IT003', name: 'Mustajab' }
-  ];
-
-  // Route to get all users
-app.get('/api/users', (req, res) => {
-    res.status(200).json(users);
-  });
-
-
-  // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  }); */
  
 // Importing required modules
 const tasksRoutes = require('./tasks')
@@ -30,4 +13,6 @@ const tasksRoutes = require('./tasks')
 app.use('/tasks', tasksRoutes);
 
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
